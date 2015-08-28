109efcbdff75dec2aa94dc3c0f9aa178fc98f728
isvidler <isvidler@umich.edu>
2015-08-28 17:41:00 -0400

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Ittai Svidler on 8/28/15.
+//  Copyright (c) 2015 Saturnup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
